1f2d12aa17262b2963d1416b043ebebbdc13a3c3
volak-klara <klaravolak@gmail.com>
2024-12-04 20:05:52 +0100

    deleted files

--- Domasna2/tech-prototype/backend/StockApp/StockApp/src/main/java/mk/finki/ukim/StockApp/exception/CompanyNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package mk.finki.ukim.StockApp.exception;
-
-public class CompanyNotFoundException extends Exception {
-    public CompanyNotFoundException(Long id) {
-        super(String.format("Company not found: %d", id));
-    }
-}
